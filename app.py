bfff3326e437c1835aaf61db425954bd7c5d33df
LilHack <43441041+Lil-hack@users.noreply.github.com>
2021-12-07 18:00:07 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 import os
-
+from instapy import InstaPy
 
 if __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 5000.
-    while True:
-        print('lox')
+
+    InstaPy(username="<your_username>", password="<your_password>").login()
+  //  while True:
+    //    print('lox')
