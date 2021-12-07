f1a7fc467642813d68e553e9c3b97832242dc30b
LilHack <43441041+Lil-hack@users.noreply.github.com>
2021-12-07 18:04:22 +0300

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,6 @@ from instapy import InstaPy
 if __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 5000.
 
-    InstaPy(username="<your_username>", password="<your_password>").login()
+    InstaPy(username="lumpyproduction@gmail.com", password="lumpybeats1996").login()
     while True:
         pass
